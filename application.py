2e8b700536e06234faeafa14ad8ef278bb5ae6f7
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2020-05-05 22:38:06 -0400

    adding files

---
+++ application.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+
+app = Flask(__name__)
+#En esta variable se almacena la ubicacion de mi base de datos y los parametros de esta
+engine = create_engine("postgresql://postgres:DAmr160684@localhost:5432/postgres")
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("index.html", flights=flights)
+
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight"""
+
+    name = request.form.get("name")
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number.")
+
+    if db.execute("SELECT * FROM flights WHERE id = id", {"id": flight_id}).rowcount == 0:
+        return render_template("error.html", message="No such flight with that id")
+    db.execute("INSERT INTO passenger (name, flight_id) VALUES (:name, :flight_id)",
+    {"name": name, "flight_id": flight_id})
+
+    db.commit()
+    return render_template("success.html")
+
+
+if __name__ == "__main__":
+    main()
